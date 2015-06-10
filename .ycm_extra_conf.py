98a5765d4f41e86edb96f9d4671233721cd9b1a7
yoloo <yoloo0624@gmail.com>
2015-06-10 14:54:20 +0800

    modify .ycm_extra_conf.py for c++

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -85,6 +85,10 @@ flags = [
 './usr/include',
 '-isystem',
 './usr/local/include',
+'-isystem',
+'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1',
+'-isystem',
+'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
 ]
 
 
