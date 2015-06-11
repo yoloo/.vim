796b91ade07d391cdd83390d9e6703555839fae7
yoloo <yoloo0624@gmail.com>
2015-06-11 21:48:18 +0800

    modify install2.sh

--- install2.sh
+++ install2.sh
@@ -18,23 +18,27 @@ then
 fi
 
 cd $tmp;
+#llvm
 wget http://llvm.org/releases/3.4/llvm-3.4.src.tar.gz;
 tar xzf llvm-3.4.src.tar.gz
 rm llvm-3.4.src.tar.gz
 mv -f llvm-3.4.src.tar.gz llvm
 
 cd ./llvm/tools;
+#clang
 wget http://llvm.org/releases/3.4/clang-3.4.src.tar.gz;
 tar xzf clang-3.4.src.tar.gz;
 rm clang-3.4.src.tar.gz;
 mv -f clang-3.4 clang
 
 cd ../projects;
+#compiler-rt
 wget http://llvm.org/releases/3.4/compiler-rt-3.4.src.tar.gz;
 tar xzf compiler-rt-3.4.src.tar.gz;
 rm compiler-rt-3.4.src.tar.gz;
 mv -f compiler-rt-3.4.src.tar.gz compiler-rt;
 
+#test-suite
 wget http://llvm.org/releases/3.4/test-suite-3.4.src.tar.gz;
 tar xzf test-suite-3.4.src.tar.gz;
 rm test-suite-3.4.src.tar.gz;
@@ -46,4 +50,4 @@ make -j2;
 sudo make install;
 
 cd $ycm_build;
-cmake -G "Unix Makefiles" ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ -DEXTERNAL_LIBCLANG_PATH=~/tmp/build/Release+Asserts/lib/libclang.so
+cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=~/tmp/build/Release+Asserts/lib/libclang.so . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ 
